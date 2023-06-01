e22d2e2087212811b574b5f66cbce2ef0f5c3475
paolo <paolo.convertini@gmial.com>
2023-06-01 14:10:40 +0200

    fix

--- authentication/src/main/java/it/calolenoci/service/TokenService.java
+++ authentication/src/main/java/it/calolenoci/service/TokenService.java
@@ -18,7 +18,7 @@ public class TokenService {
         Set<String> roles = user.roles.stream()
                 .map(role -> role.name)
                 .collect(Collectors.toSet());
-        long l = (System.currentTimeMillis() / 1000) + 3600;
+        long l = (System.currentTimeMillis() + 86400000) / 1000;
         JwtClaimsBuilder builder = Jwt.upn(user.username);
         if(roles.contains("Venditore")){
             builder.claim(Claims.nickname, user.codVenditore);
