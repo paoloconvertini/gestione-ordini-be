54643fef5f4d696055cdd902f95a20a258babde6
paolo <paolo.convertini@gmial.com>
2023-11-14 15:18:54 +0100

    script

---
+++ scripts/ALTER_GO_CESPITE_V2.sql
@@ -0,0 +1,11 @@
+alter table GO_CESPITE
+    add
+        COSTO_GRUPPO int,
+        COSTO_CONTO varchar(6),
+        FONDO_GRUPPO int,
+        FONTO_CONTO varchar(6),
+        PLUS_GRUPPO int,
+        PLUS_CONTO varchar(6),
+        MINUS_GRUPPO int,
+        MINUS_CONTO varchar(6)
+    go
