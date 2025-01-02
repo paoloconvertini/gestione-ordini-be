27d08f1c587bc3cb25725080a51b59266df0b752
paolo <paolo.convertini@gmial.com>
2025-01-02 22:28:59 +0100

    OAF fix

--- gestione-ordini-api/src/main/java/it/calolenoci/service/OrdineFornitoreService.java
+++ gestione-ordini-api/src/main/java/it/calolenoci/service/OrdineFornitoreService.java
@@ -80,7 +80,7 @@ public class OrdineFornitoreService {
             fornitori.addAll(mapArticoli.keySet());
             int index = 1;
             Integer progressivoForn = OrdineFornitore.find("SELECT CASE WHEN MAX(progressivo) IS NULL THEN 0 ELSE MAX(progressivo) END FROM OrdineFornitore o WHERE o.anno = :a", Parameters.with("a", LocalDate.now().getYear())).project(Integer.class).firstResult();
-            Integer progressivoFornDettaglio = OrdineFornitoreDettaglio.find("SELECT CASE WHEN MAX(progrGenerale) IS NULL THEN 0 ELSE MAX(progrGenerale) END FROM OrdineFornitoreDettaglio o WHERE o.anno = :a", Parameters.with("a", LocalDate.now().getYear())).project(Integer.class).firstResult();
+            Integer progressivoFornDettaglio = OrdineFornitoreDettaglio.find("SELECT CASE WHEN MAX(progrGenerale) IS NULL THEN 0 ELSE MAX(progrGenerale) END FROM OrdineFornitoreDettaglio").project(Integer.class).firstResult();
             List<RegistroAzioni> registroAzioniList = new ArrayList<>();
             for (String sottoConto : mapArticoli.keySet()) {
                 List<ArticoloDto> articoloDtoList = mapArticoli.get(sottoConto);
