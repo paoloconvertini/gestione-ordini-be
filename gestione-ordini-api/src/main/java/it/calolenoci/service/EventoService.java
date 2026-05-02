223175cbc60c62d8255d7460bfa92f1e01c5b152
paolo <paolo.convertini@gmial.com>
2026-05-02 09:34:02 +0200

    fix storni

--- gestione-ordini-api/src/main/java/it/calolenoci/service/EventoService.java
+++ gestione-ordini-api/src/main/java/it/calolenoci/service/EventoService.java
@@ -13,7 +13,20 @@ public class EventoService {
 
     public List<RegistroAzioniDto> getByAnnoSerieProgressivoRigo(Integer anno, String serie, Integer progressivo, Integer rigo) {
         return RegistroAzioni
-                .find("Select r FROM RegistroAzioni r WHERE anno = :anno AND serie = :serie AND progressivo =: progressivo AND rigo = :rigo",
+                .find("Select " +
+                                "r.anno as anno, " +
+                                "r.serie as serie, " +
+                                "r.progressivo as progressivo, " +
+                                "r.rigo as rigo, " +
+                                "r.username as username, " +
+                                "r.createDate as createDate, " +
+                                "r.azione as azione, " +
+                                "r.quantita as quantita, " +
+                                "r.tono as tono, " +
+                                "r.qtaRiservata as qtaRiservata, " +
+                                "r.qtaProntoConsegna as qtaProntoConsegna " +
+                                "FROM RegistroAzioni r " +
+                                "WHERE anno = :anno AND serie = :serie AND progressivo =: progressivo AND rigo = :rigo",
                         Sort.descending("createDate"),
                 Parameters.with("anno", anno).and("serie", serie)
                         .and("progressivo", progressivo).and("rigo", rigo)).project(RegistroAzioniDto.class).list();
